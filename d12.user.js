73cc0f505f12670a8ca684a881b4269715e9fb17
Greg Cochard <gcochard@linkedin.com>
2016-02-12 09:33:26 -0800

    Use slack name

--- d12.user.js
+++ d12.user.js
@@ -4,7 +4,7 @@
 // @updateURL    https://gist.githubusercontent.com/gcochard/1b6e94b6ae6e2f60a6d8/raw/d12.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.0.0/lodash.min.js
 // @require      https://npmcdn.com/dive-buddy
-// @version      1.4.2
+// @version      1.4.3
 // @description  calls hubot with the current player and other features
 // @author       Greg Cochard
 // @match        http://dominating12.com/game/*
@@ -158,7 +158,10 @@ $(document).ready(function(){
 
     function signalStartToHubot(starter){
         'use strict';
-    
+        starter = users[starter];
+        if(!starter){
+            return;
+        }
         $.ajax({
             url: hubotLocation+'pushstart',
             method: 'POST',
