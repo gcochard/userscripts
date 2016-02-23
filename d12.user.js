6432fd53de58d36b34d0845f026538bddd5d0952
Greg Cochard <gcochard@linkedin.com>
2016-02-23 08:48:47 -0800

    Fix fog detection on dice

--- d12.user.js
+++ d12.user.js
@@ -4,7 +4,7 @@
 // @updateURL    https://gist.githubusercontent.com/gcochard/1b6e94b6ae6e2f60a6d8/raw/d12.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.0.0/lodash.min.js
 // @require      https://npmcdn.com/dive-buddy
-// @version      1.5.1
+// @version      1.5.2
 // @description  calls hubot with the current player and other features
 // @author       Greg Cochard
 // @match        http://dominating12.com/game/*
@@ -397,7 +397,7 @@ $(document).ready(function(){
                     return;
                 }
                 var dicehtml = dice.map(function(roll){
-                    if(roll.player !== player && fog){
+                    if(roll.player !== player && !fog){
                         return;
                     }
                     var color = colorDice(roll);
