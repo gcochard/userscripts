749de03f602f66c7a2d9c139d9eea41a2887d97c
Greg Cochard <gcochard@linkedin.com>
2016-03-21 13:54:59 -0700

    bug fix

--- d12.user.js
+++ d12.user.js
@@ -4,7 +4,7 @@
 // @updateURL    https://gist.githubusercontent.com/gcochard/1b6e94b6ae6e2f60a6d8/raw/d12.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.0.0/lodash.min.js
 // @require      https://npmcdn.com/dive-buddy
-// @version      1.5.5
+// @version      1.5.6
 // @description  calls hubot with the current player and other features
 // @author       Greg Cochard
 // @match        http://dominating12.com/game/*
@@ -431,6 +431,8 @@ $(document).ready(function(){
         signalStartToHubot(getStarter());
     }
 
+    var me = detectMe();
+
     function populateDiceGui(dice){
         var oldCount = $('#dice li').length, newCount = 0;
         if(!(dice instanceof Array)){
@@ -438,7 +440,7 @@ $(document).ready(function(){
             return;
         }
         var dicehtml = dice.map(function(roll){
-            if(roll.player !== player && !fog){
+            if(roll.player !== me && fog){
                 return;
             }
             var color = colorDice(roll);
