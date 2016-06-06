6170a03cb826ad14a2e04f70abde87c4f5d3313e
Greg Cochard <gcochard@users.noreply.github.com>
2016-06-06 11:33:05 -0700

    Fix sendAttack fix

--- d12.user.js
+++ d12.user.js
@@ -4,7 +4,7 @@
 // @updateURL    https://gist.githubusercontent.com/gcochard/1b6e94b6ae6e2f60a6d8/raw/d12.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.0.0/lodash.min.js
 // @require      https://npmcdn.com/dive-buddy
-// @version      1.6.20
+// @version      1.6.21
 // @description  calls hubot with the current player and other features
 // @author       Greg Cochard
 // @match        http://dominating12.com/game/*
@@ -550,29 +550,6 @@ $(document).ready(function(){
 
     var signalTurnStartToHubot = signalChangeToHubot.bind(null,'startturn');
     var signalTurnEndToHubot = signalChangeToHubot.bind(null,'endturn');
-
-    //var oldSendAttack = playGame.sendAttack;
-    playGame.sendAttack = function(){
-        var ab = $('.attack-button');
-        ab.attr('disabled',true).off('click').removeAttr('onclick');
-        return this.ajax('attack', {
-            'territory_from': this.selectedTerritory,
-            'territory_to': this.targetedTerritory
-        }, (function(_this) {
-            return function(result) {
-                ab.removeAttr('disabled').off('click').on('click',playGame.sendAttack.bind(playGame));
-                if (result.success === true) {
-                    _this.runUpdates(result);
-                    if (_this.status !== 3) {
-                        return _this.updateTurn(result.turn, _this.gameState.territories[_this.selectedTerritory].troops === 1);
-                    }
-                } else {
-                    _this.reportError(result.error);
-                    return _this.initializeTurn();
-                }
-            };
-        })(this));
-    };
 
     var oldBeginTurn = playGame.beginTurn;
     playGame.beginTurn = function(){
