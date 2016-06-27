1c829115a2cbe50b5883078cb1de40f066ad59e7
Greg Cochard <gcochard@users.noreply.github.com>
2016-06-27 10:10:37 -0700

    Bump version

--- d12.user.js
+++ d12.user.js
@@ -4,7 +4,7 @@
 // @updateURL    https://github.gregcochard.com/userscripts/d12.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.0.0/lodash.min.js
 // @require      https://npmcdn.com/dive-buddy
-// @version      1.6.27
+// @version      1.6.28
 // @description  calls hubot with the current player and other features
 // @author       Greg Cochard
 // @match        http://dominating12.com/game/*
