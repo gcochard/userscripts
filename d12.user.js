2c16404552b9fbe33b8f6224e6bd3d34c5089d22
Greg Cochard <gcochard@users.noreply.github.com>
2019-11-19 09:13:46 -0800

    Fix me detection
    The `pull-left` class is gone.

--- d12.user.js
+++ d12.user.js
@@ -4,7 +4,7 @@
 // @updateURL    https://github.gregcochard.com/userscripts/d12.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.0.0/lodash.min.js
 // @require      https://unpkg.com/dive-buddy
-// @version      1.7.3
+// @version      1.7.4
 // @description  calls hubot with the current player and other features
 // @author       Greg Cochard
 // @match        http://dominating12.com/game/*
@@ -95,7 +95,7 @@ $(document).ready(function(){
         me = diveBuddy(playGame,'me.username','');
 
         if(!me){
-            me = $('.nav-list.pull-left a:first').text();
+            me = $('.nav-list a:first').text();
         }
         console.log('me: %s',me);
         return me;
